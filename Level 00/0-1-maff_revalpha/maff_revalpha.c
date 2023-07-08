92fbd05baeaddc75a10682903c76522d30a30d86
aoubaidi <114623303+aoubaidi@users.noreply.github.com>
2023-07-08 17:49:26 +0100

    Update maff_revalpha.c

--- Level 00/0-1-maff_revalpha/maff_revalpha.c
+++ Level 00/0-1-maff_revalpha/maff_revalpha.c
@@ -7,23 +7,24 @@
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/14 12:30:57 by fwuensch          #+#    #+#             */
 /*   Updated: 2017/07/14 12:30:58 by fwuensch         ###   ########.fr       */
+/*   Updated: 2023/07/08 17:49:20 by aoubaidi                                 */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putchar(char c)
+int		main(int argc,char *argv[])
 {
-	write(1, &c, 1);
-}
-
-int		main(void)
-{
-	int i;
-
-	i = 123;
-	while (i-- > 97)
-		(i % 2 == 0) ? ft_putchar(i) : ft_putchar(i - 32);
-	ft_putchar('\n');;
+	int i=90;
+	char c;
+	while(i>64){
+		if(i%2){
+			c=i;
+		}
+		else c=i+32;
+		write(1,&c,1);
+		i--;
+	}
+	write(1,"\n",1);
 	return (0);
 }
